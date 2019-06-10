1029027a1c976232fb8d79a19fe886f20e771a47
kindy <380149740@qq.com>
2019-06-10 16:45:10 +0800

    a

---
+++ decorator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/10 16:43
+# @Author  : jindi
+# @FileName: decorator.py
+
+def hello():
+    print('hello')
+
+if __name__=='__main__':
+    hello()
+
+
